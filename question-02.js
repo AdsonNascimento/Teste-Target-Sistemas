1beda86818ed175c7c9b47d75fce825492120d25
Adson Nascimento <adson.nascimento.112@ufrn.edu.br>
2023-03-07 23:13:30 -0300

    01 a 04

---
+++ question-02.js
@@ -0,0 +1,18 @@
+function fibonacci(x) {
+    let a = 0, b = 1;
+    let y = x + 1;
+    
+    for (let i = 0; i < y; i++) {
+      let cd = a;
+      a = b;
+      b = cd + b;
+    }
+  
+    if (x == a) {
+      return `número ${x} pertence a sequência`;
+    }
+    
+    return `número ${x} não pertence a sequência`;
+}
+  
+console.log(fibonacci(12));  
---
+++ question-03.js
@@ -0,0 +1,21 @@
+//letra a
+function gauss1(n) {
+    const a1 = 1, r = 2;
+    let pa = a1 + (n-1)*r;
+  return pa;
+  }
+  
+  console.log(gauss1(5));
+  
+  //letra b
+  function gauss2(n) {
+    const a1 = 2, r = 2;
+    let tg = (a1*(r**(n-1)));
+  return tg;
+  }
+  
+  console.log(gauss2(7));
+  
+  //letra c
+  
+  //letra d
